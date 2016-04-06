4173b02918f4008084e1a77fb9c56a63215c0ca9
yoni <yoni14504@gmail.com>
2016-04-06 17:11:22 +0300

    שינוי גדול

---
+++ GroupBuyServer/GroupBuyServer/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using GroupBuyServer.Models;
+using Newtonsoft.Json.Linq;
+
+namespace GroupBuyServer.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Login(JObject jsonLoginDetails)
+        {
+            string strUserName = jsonLoginDetails["userName"].Value<string>();
+            string strPassword = jsonLoginDetails["password"].Value<string>();
+
+            return this.Ok(new User());
+        }
+    }
+}
