a58e8f86fad58b5b7d131d7326f2f73c7bc3e9a1
Amit Messing <amitmessing16@gmail.com>
2016-05-13 14:40:53 +0300

    תיקון קטן בהירשמות

--- GroupBuyServer/GroupBuyServer/Controllers/RegisterController.cs
+++ GroupBuyServer/GroupBuyServer/Controllers/RegisterController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Routing;
 using GroupBuyServer.Models;
 using GroupBuyServer.Utils;
 using Newtonsoft.Json.Linq;
 
 namespace GroupBuyServer.Controllers
 {
-    [RoutePrefix("/GroupBuyServer/api/register/")]
+    [RoutePrefix("/GroupBuyServer/api/register")]
     public class RegisterController : ApiController
     {
         [HttpPost]
