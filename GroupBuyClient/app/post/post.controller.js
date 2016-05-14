383cf2ce514261f7fcc86a1f56864137c8ffb22d
Amit Messing <amitmessing16@gmail.com>
2016-05-14 12:56:00 +0300

    כמה תוספות למסך הוספת פוסט

--- GroupBuyClient/app/post/post.controller.js
+++ GroupBuyClient/app/post/post.controller.js
@@ -11,5 +11,21 @@ mainApp
                 value: 20
             }];
 
+            var imageLoader = document.getElementById('imageLoader');
+            imageLoader.addEventListener('change', handleImage, false);
+            var canvas = document.getElementById('imageCanvas');
+            var ctx = canvas.getContext('2d');
+
+            function handleImage(e) {
+                var reader = new FileReader();
+                reader.onload = function (event) {
+                    var img = new Image();
+                    img.onload = function () {
+                        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+                    }
+                    img.src = event.target.result;
+                }
+                reader.readAsDataURL(e.target.files[0]);
+            }
         }
     ]);
