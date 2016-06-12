a31f3f9af521d066dfaba01e593d7fa694f88d1a
Amit Messing <amitmessing16@gmail.com>
2016-06-12 11:32:35 +0300

    תיקון התחברות

--- GroupBuyClient/services/userService.js
+++ GroupBuyClient/services/userService.js
@@ -1,18 +1,18 @@
-﻿mainApp.service('userService', ['$cookies', '$state', function ($cookies, $state) {
+﻿mainApp.service('userService', ['$cookies', '$window', '$state', function ($cookies,$window, $state) {
     var setLoggedUSer = function (usr) {
-        $cookies.put("loggedUser", JSON.stringify(usr));
+        $window.sessionStorage.setItem("loggedUser", JSON.stringify(usr));
     };
 
     var getLoggedUser = function () {
-        var user = $cookies.get("loggedUser");
-        if (user) {
+        var user = $window.sessionStorage.getItem("loggedUser");
+        if (user && user !== "undefined") {
             return JSON.parse(user);
         }
         return null;
     };
 
     var logout = function () {
-        $cookies.remove("loggedUser");
+        $window.sessionStorage.removeItem("loggedUser");
         $state.go('home');
     };
 
